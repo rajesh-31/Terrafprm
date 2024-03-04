20e1f5bc3f127786700ed80f308516a291acc8ca
rajesh-31 <136876189+rajesh-31@users.noreply.github.com>
2024-03-04 21:37:33 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -15,6 +15,22 @@ resource "aws_key_pair" "example_key_pair" {
   public_key = file("~/.ssh/id_rsa.pub")  # Replace with the path to your public key
 }
 
+ resource "aws_security_group" "example_security_group" {
+  name        = "example-security-group"
+  description = "Example security group with dynamic ingress rules"
+  vpc_id      = "vpc-0dfbf472a2d5102d3" # Replace with your VPC ID
+
+   dynamic "ingress" {
+    for_each = [22, 88, 3306, 443]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]  # Allow traffic from any IP address (adjust as needed)
+    }
+  }
+}
+
 resource "aws_instance" "app_server" {
   ami            = "ami-03f4878755434977f"
   instance_type  = "t2.micro"
