19f413c02cb25b0a54633105e3e1221a0eb446e4
rajesh-31 <136876189+rajesh-31@users.noreply.github.com>
2024-03-04 20:43:27 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,21 +1,26 @@
-# Specify the AWS provider and region
-provider "aws" {
-  region = "us-east-1"  # Change this to your desired AWS region
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
 }
 
 # Define an AWS key pair for SSH access
 resource "aws_key_pair" "example_key_pair" {
   key_name   = "example-key-pair"
-  public_key = file("~/.ssh/your-public-key.pub")  # Replace with the path to your public key
+  public_key = file("~/.ssh/id_rsa.pub")  # Replace with the path to your public key
 }
 
-# Define an AWS EC2 instance
-resource "aws_instance" "example_instance" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Specify the desired Amazon Machine Image (AMI)
-  instance_type = "t2.micro"                # Specify the instance type
-
+resource "aws_instance" "app_server" {
+  ami           = "ami-03f4878755434977f"
+  instance_type = "t2.micro"
   key_name      = "${aws_key_pair.example_key_pair.key_name}"  # Associate the key pair with the instance
-
+  
   tags = {
-    Name = "example-instance"
+    Name = "ExampleInstance"
   }
+}
