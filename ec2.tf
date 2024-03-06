8a857a54f950bcccf67f6da51846897f627c5f2d
rajesh-31 <136876189+rajesh-31@users.noreply.github.com>
2024-03-06 12:51:25 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -33,19 +33,4 @@ resource "aws_instance" "app_server" {
   tags = {
     Name = "MyTerraformInstance"
   }
-   user_data = <<-EOF
-              #!/bin/bash
-              sudo apt update -y          # Update the package repository (for ubuntu)
-              sudo apt-get install -y openjdk-17-jre-headless  # Install Java
-              sudo apt-get install -y python3    # Install Python3
-              curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-/usr/share/keyrings/jenkins-keyring.asc > /dev/null
-             echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-/etc/apt/sources.list.d/jenkins.list > /dev/null 
-              sudo apt-get update
-              sudo apt-get install -y jenkins   # Install Jenkins
-              sudo systemctl start jenkins  # Start Jenkins
-              sudo systemctl enable jenkins   # Enable Jenkins to start on boot
-              EOF
 }
